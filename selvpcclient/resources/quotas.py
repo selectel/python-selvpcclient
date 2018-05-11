f5ed374f528a80fa47921d760c37d8d80db67199
bititanb <bititanc@gmail.com>
2018-05-11 18:37:13 +0300

    Fix trying to optimize volume_gigabytes_iso resource (#18) (#19)
    The resource is dynamic/virtual and should not be changed/optimized.
    
    https://github.com/selectel/python-selvpcclient/issues/18

--- selvpcclient/resources/quotas.py
+++ selvpcclient/resources/quotas.py
@@ -90,6 +90,12 @@ class QuotasManager(base.Manager):
         body = {"quotas": {}}
         quotas = self.get_project_quotas(project_id)._info
         for resource, quotas_ in quotas.items():
+            # dynamic resource, can't be set manually
+            # see issue for details:
+            # https://github.com/selectel/python-selvpcclient/issues/18
+            if resource == "volume_gigabytes_iso":
+                continue
+
             for quota in quotas_:
                 if quota["value"] == 0 or quota["value"] == quota["used"]:
                     continue
